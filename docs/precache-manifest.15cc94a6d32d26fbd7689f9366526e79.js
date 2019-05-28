25e2afeda0a058fa1cf1f33b426172928badeb25
Northwalker <northwalker28@gmail.com>
2019-05-29 00:01:14 +0800

    Update dependencies

---
+++ docs/precache-manifest.15cc94a6d32d26fbd7689f9366526e79.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ca730b9bcef89d3e95c",
+    "url": "/vue-hotel-datepicker/js/app.fc813f7b.js"
+  },
+  {
+    "revision": "ea0842dae1698adb321e",
+    "url": "/vue-hotel-datepicker/js/chunk-vendors.e2d23fa3.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-hotel-datepicker/img/vue-logo.82b9c7a5.png"
+  },
+  {
+    "revision": "2722166e5997867dba39f80701930abe",
+    "url": "/vue-hotel-datepicker/index.html"
+  }
+];

a83435c9d18ef57e9d8a990f43403ae61aee56a2
Northwalker <northwalker28@gmail.com>
2019-08-13 11:48:42 +0800

    Fixed issue #8 and #9

---
+++ docs/precache-manifest.5f7168538c0ffc64e83f4c456ff9dc19.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "0739763f31ea212a7803",
+    "url": "/vue-hotel-datepicker/js/app.6c3e46a3.js"
+  },
+  {
+    "revision": "5ef125f3e3f8ab3d9f7d",
+    "url": "/vue-hotel-datepicker/js/chunk-vendors.61bc5260.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-hotel-datepicker/img/vue-logo.82b9c7a5.png"
+  },
+  {
+    "revision": "8de2a0558e06daf50042eea66b953348",
+    "url": "/vue-hotel-datepicker/index.html"
+  }
+];
